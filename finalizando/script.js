8be7c4ea6b13f143aace593f1d93aea6f97f8c47
Tiago Braga <tiagohbraga@gmail.com>
2022-10-24 16:13:27 -0300

    atualização tentando colocar texto

--- finalizando/script.js
+++ finalizando/script.js
@@ -1,6 +1,13 @@
 
 function meuCarro() {
+    
     const meusCarros = document.getElementById("carros")
-    console.log(meusCarros)
+    const selectedCar = meusCarros.options[meusCarros.selectedIndex].text
+    const imgCar = '<img src="./imagens/imagemBusca/' + selectedCar + '-1.jpg">';
+    document.getElementById("conteudo").innerHTML = "<h1>" + selectedCar + "<h1>" + imgCar;
+    console.log(selectedCar)
+
 }
 
+
+// 
